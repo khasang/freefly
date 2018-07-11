3cb6dd7759956f438785306a9b41098db4fc3b3f
skuznet <azon.sk@gmail.com>
2018-07-11 21:07:11 +0300

    add spring core

---
+++ src/main/java/io/khasang/freefly/controller/AppController.java
@@ -0,0 +1,15 @@
+package io.khasang.freefly.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// controller MVC
+@Controller
+// localhost:8080 /
+public class AppController {
+
+    @RequestMapping("/")
+    public String getHelloPage() {
+        return "hello";
+    }
+}
