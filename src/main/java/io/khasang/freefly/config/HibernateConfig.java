d5bcf7f22ab1a67020750ca49fd2b6a07d311767
kvv <v.kochurov@rambler.ru>
2018-07-29 21:26:26 +0300

    add jdbc and spring-security. app fails on http://localhost:8080/password/user. in order to create encoder for password -  hardcod getCriptPasswordForAdminAndUser

---
+++ src/main/java/io/khasang/freefly/config/HibernateConfig.java
@@ -0,0 +1,67 @@
+package io.khasang.freefly.config;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@PropertySource(value = "classpath:hibernate.properties")
+public class HibernateConfig {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getRequiredProperty("hibernate.driver"));
+        dataSource.setUrl(environment.getRequiredProperty("hibernate.url"));
+        dataSource.setUsername(environment.getRequiredProperty("hibernate.user"));
+        dataSource.setPassword(environment.getRequiredProperty("hibernate.password"));
+        return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory(){
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("io.khasang.freefly.entity");
+        sessionFactory.setHibernateProperties(properties());
+        return sessionFactory;
+    }
+
+    private Properties properties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect",environment.getRequiredProperty("hibernate.dialect"));
+        properties.setProperty("hibernate.show_sql",environment.getRequiredProperty("hibernate.show_sql"));
+        properties.setProperty("hibernate.format_sql",environment.getRequiredProperty("hibernate.format_sql"));
+        properties.setProperty("hibernate.hbm2dll.auto",environment.getRequiredProperty("hibernate.hbm2dll.auto"));
+        properties.setProperty("show_sql",environment.getRequiredProperty("hibernate.show_sql"));
+        return properties;
+    }
+
+    @Bean
+    @Autowired
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory){
+        HibernateTransactionManager hibernateTransactionManager = new HibernateTransactionManager();
+        hibernateTransactionManager.setSessionFactory(sessionFactory);
+        return hibernateTransactionManager;
+    }
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+}
---
+++ src/main/java/io/khasang/freefly/entity/Cat.java
@@ -0,0 +1,50 @@
+package io.khasang.freefly.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cats")
+public class Cat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+    private String description;
+
+    @Column(name = "color_id")
+    private int colorId;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getColorId() {
+        return colorId;
+    }
+
+    public void setColorId(int colorId) {
+        this.colorId = colorId;
+    }
+}
