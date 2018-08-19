f3e9eff9c0bdf566d5f438545848da3d46201893
skuznet <azon.sk@gmail.com>
2018-08-19 20:37:30 +0300

    add example for criteria query by custom field

--- src/main/java/io/khasang/freefly/dao/CatDao.java
+++ src/main/java/io/khasang/freefly/dao/CatDao.java
@@ -2,5 +2,9 @@ package io.khasang.freefly.dao;
 
 import io.khasang.freefly.entity.Cat;
 
+import java.util.List;
+
 public interface CatDao extends BasicDao<Cat>{
+
+    List<Cat> getCatsByName(String name);
 }
