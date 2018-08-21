fa146183c60ac1a15c5dc3141d059847b9bce200
Lenar S <samatov_lr@mail.ru>
2018-08-21 21:57:21 +0400

    feature/frf0027 add user rest layer

---
+++ src/main/java/io/khasang/freefly/dto/UserDTO.java
@@ -0,0 +1,106 @@
+package io.khasang.freefly.dto;
+
+import io.khasang.freefly.entity.User;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String login;
+    private String password;
+    private boolean isLock;
+
+    public List<UserDTO> getUserDTOList(List<User> userList) {
+        List<UserDTO> userDTOList = new ArrayList<>();
+
+        for (User user : userList) {
+            UserDTO userDTO = new UserDTO();
+            userDTO.setId(user.getId());
+            userDTO.setFirstName(user.getFirstName());
+            userDTO.setLastName(user.getLastName());
+            userDTO.setEmail(user.getEmail());
+            userDTO.setLogin(user.getLogin());
+            userDTO.setPassword(user.getPassword());
+            userDTO.setLock(user.isLock());
+
+            userDTOList.add(userDTO);
+        }
+
+        return userDTOList;
+    }
+
+    public UserDTO getUserDTO(User user) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(user.getId());
+        userDTO.setFirstName(user.getFirstName());
+        userDTO.setLastName(user.getLastName());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setLogin(user.getLogin());
+        userDTO.setPassword(user.getPassword());
+        userDTO.setLock(user.isLock());
+
+        return userDTO;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isLock() {
+        return isLock;
+    }
+
+    public void setLock(boolean lock) {
+        isLock = lock;
+    }
+}
