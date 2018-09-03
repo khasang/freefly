341fe6de5e39349a3cff4ba42ab57fe037d672c3
ArthurM2 <arthur.komarov@gmail.com>
2018-09-03 23:59:25 +0500

    add SOAP Client example

--- src/main/java/io/khasang/freefly/controller/AppController.java
+++ src/main/java/io/khasang/freefly/controller/AppController.java
@@ -3,6 +3,7 @@ package io.khasang.freefly.controller;
 import io.khasang.freefly.model.Call;
 import io.khasang.freefly.model.CreateTable;
 import io.khasang.freefly.model.Message;
+import io.khasang.freefly.util.CheckText;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.annotation.Secured;
@@ -12,6 +13,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.net.MalformedURLException;
 
 // controller MVC
 @Controller
@@ -22,6 +26,8 @@ public class AppController {
     private final Message message;
     private final Call callImpl;
     private final CreateTable createTable;
+    @Autowired
+    private CheckText checkText;
 
     @Autowired
     public AppController(Call callImpl, Message message, CreateTable createTable) {
@@ -61,5 +67,11 @@ public class AppController {
         model.addAttribute("password", password);
         model.addAttribute("encodePassword", new BCryptPasswordEncoder().encode(password));
         return "password";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/check/{text}", method = RequestMethod.GET)
+    public String checkText(@PathVariable("text") String text) throws MalformedURLException {
+        return checkText.checkWord(text);
     }
 }
---
+++ src/main/java/io/khasang/freefly/util/CheckText.java
@@ -0,0 +1,36 @@
+package io.khasang.freefly.util;
+
+import net.yandex.speller.services.spellservice.CheckTextRequest;
+import net.yandex.speller.services.spellservice.SpellServiceSoap;
+import org.springframework.stereotype.Component;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+@Component
+public class CheckText {
+
+    private static final String ADDRESS = "http://speller.yandex.net/services/spellservice?WSDL";
+
+    public String checkWord(String text) throws MalformedURLException {
+        URL url = new URL(ADDRESS);
+
+        QName qName = new QName("http://speller.yandex.net/services/spellservice", "SpellService");
+        Service service = Service.create(url, qName);
+
+        SpellServiceSoap spellService = service.getPort(SpellServiceSoap.class);
+
+        CheckTextRequest request = new CheckTextRequest();
+        request.setText(text);
+        request.setLang("en");
+        request.setFormat("plain");
+
+        if(spellService.checkText(request).getSpellResult().getError().size() !=0) {
+            return spellService.checkText(request).getSpellResult().getError().get(0).getS().toString();
+        } else {
+            return "Word correct";
+        }
+    }
+}
